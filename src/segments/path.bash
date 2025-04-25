b3822c9ee8feaf8369eb5b3fb6e8b4763ca2633f
d-Dudas <david.dudas03@e-uvt.ro>
2025-04-25 11:12:24 +0300

    Remove path shorting.

--- src/segments/path.bash
+++ src/segments/path.bash
@@ -5,12 +5,6 @@ segments::path() {
 
   local wdir=${PWD/${HOME}/\~}
 
-  if [[ ${#wdir} -gt $segment_max_length ]]; then
-    folder=${wdir##*/}
-    IFS='/' wdir=$(for p in ${wdir}; do printf '%s/' "${p:0:1}"; done)
-    wdir="${wdir%/*}${folder:1}"
-  fi
-
   IFS=/ read -r -a wdir_array <<<"${wdir}"
   if [[ $SEGMENTS_PATH_SPLITTER_DISABLE -ne 1 && ${#wdir_array[@]} -gt 1 ]]; then
     declare -a segments
